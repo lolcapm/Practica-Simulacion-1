c04d4ec0f97bb9df5beec80f7424da000fc5cad4
lolcapm <amayberc@gmail.com>
2018-10-12 21:09:23 -0400

    Practica de Simulacion Vuforia y VR

---
+++ Practica 1 Programacion Grafica 2/Assets/ControlBoton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlBoton : MonoBehaviour {
+	public void botones(){
+		SceneManager.LoadScene (1);
+	}
+
+}
